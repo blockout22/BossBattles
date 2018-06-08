7b04c2c30ef43eb8ba6c52f36e46ca33af166b90
blockout22 <kie1k93@gmail.com>
2018-06-08 18:18:31 +0100

    setup core class

--- core/src/com/blockout22/rpg/boss/battles/RPGBossBattles.java
+++ core/src/com/blockout22/rpg/boss/battles/RPGBossBattles.java
@@ -1,33 +1,46 @@
 package com.blockout22.rpg.boss.battles;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kotcrab.vis.ui.VisUI;
 
-public class RPGBossBattles extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class RPGBossBattles extends Game {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+	    //stop the back key doing anything (prevents some glitches in Libgdx)
+	    Gdx.input.setCatchBackKey(true);
+
+        VisUI.load(VisUI.SkinScale.X2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		super.render();
 	}
-	
-	@Override
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	    VisUI.dispose();
 	}
 }
